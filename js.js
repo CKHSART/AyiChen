8420cdf64bcf0dffff1b0c6422acd936b47f00ee
CKHSART <fa125@gl.ck.tp.edu.tw>
2026-07-07 14:07:41 +0800

    move some function

--- js.js
+++ js.js
@@ -483,23 +483,6 @@ function openIntroBox(i) {
     const myModal = document.getElementById('introBox');
     const body = document.body;
     prepIntroBox(i);
-    myModal.onclick = function(event) {
-        // 取得 dialog 實體方塊的四個邊界座標
-        const rect = myModal.getBoundingClientRect();
-        
-        // 判斷滑鼠點擊的位置是否在 dialog 實體方塊外面
-        const isClickOutside = (
-            event.clientX < rect.left ||
-            event.clientX > rect.right ||
-            event.clientY < rect.top ||
-            event.clientY > rect.bottom
-        );
-        
-        // 如果點在外面，就執行關閉
-        if (isClickOutside) {
-            closeIntroBox();
-        }
-    };
     myModal.showModal(); // 原生方法：自動鎖定背景互動
     body.classList.add('modal-open'); // 鎖定背景滾動
 }
@@ -518,24 +501,6 @@ function showRoute() {
     const element_introBoxDiv = document.getElementById("introBoxDiv");
     element_introBoxDiv.replaceChildren();
     element_introBoxDiv.appendChild(imageWrapper);
-    
-    myModal.onclick = function(event) {
-        // 取得 dialog 實體方塊的四個邊界座標
-        const rect = myModal.getBoundingClientRect();
-        
-        // 判斷滑鼠點擊的位置是否在 dialog 實體方塊外面
-        const isClickOutside = (
-            event.clientX < rect.left ||
-            event.clientX > rect.right ||
-            event.clientY < rect.top ||
-            event.clientY > rect.bottom
-        );
-        
-        // 如果點在外面，就執行關閉
-        if (isClickOutside) {
-            closeIntroBox();
-        }
-    };
     myModal.showModal(); // 原生方法：自動鎖定背景互動
     body.classList.add('modal-open'); // 鎖定背景滾動
 }
@@ -616,13 +581,3 @@ function prepIntroBox(i) {
     element_introBoxDiv.appendChild(imageWrapper);
     element_introBoxDiv.appendChild(introDiv);
 }
-
-document.addEventListener('keydown', function(event) {
-    // 檢查按下的鍵是否為 ESC (Escape)
-    if (event.key === 'Escape') {
-        const myModal = document.getElementById('introBox');
-        const body = document.body;
-        myModal.close(); // 原生方法：關閉視窗
-        body.classList.remove('modal-open'); // 解除背景滾動鎖定
-    }
-});
